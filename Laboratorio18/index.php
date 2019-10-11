425739c0ae0aa54f4c29e5deb9e9f3789bdfe7ca
AdrizPaola <46755288+AdrizPaola@users.noreply.github.com>
2019-10-10 22:35:01 -0500

    Implementando AJAX para búsqueda en BD

---
+++ Laboratorio18/index.php
@@ -0,0 +1,12 @@
+<?php
+    require_once("util.php");
+    encabezado();
+    indice();
+    ajax();
+    agregarFruta();
+    eliminarFruta();
+    editarFruta();
+    datosNombre();
+    datosPrecio();
+    footer();
+?>
